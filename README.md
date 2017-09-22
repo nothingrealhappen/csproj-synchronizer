d84f4aee8e5e68546d7099e3e1e34a720be6c91e
YaodongLi <ydli@thoughtworks.com>
2017-09-22 10:49:46 +0800

    Add version 1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+csproj-synchronizer
+--------
+## Goal
+1. check csproj file's content, make sure all <Content Include="sompath/foo/bar.js"><//Content> file is exist in project. if not exit with code 1.
+2. check all file is added into csproj file as index. if not throw error with exit code 1.
+3. [todo] Automaticly fix issues and overwrite back to csproj file.
+
+## Usage
+1. `npm i -g csproj-synchronizer`
+2. run on your project root `csproj-sync --file="path/to/your/Some.csproj" --configFile="./some/json/config/file.json"`
+
+### example config file
+```
+{
+  "checkPath": [
+     "./Source/Web/Scripts/**/*.js",
+     "./Source/Web/Content/**/*.scss",
+     "./Source/Web/Images/**/*.{png,jpg,svg}"
+  ],
+  "ignorePath": [
+    "./Source/Web/Scripts/Dest/**",
+  ]
+}
+```
---
+++ bin/index.js
@@ -0,0 +1,187 @@
+#! /usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const xml2js = require('xml2js');
+const args = require('args');
+const _ = require('lodash');
+const glob = require('glob');
+/* const yesno = require('yesno');
+ * */
+args
+    .option('file', 'the csproj file you want to sync')
+/* .option('autofix', 'automatic fix csproj file')*/
+    .option('configFile', 'the config for scan files need include in csproj');
+
+const flags = args.parse(process.argv);
+
+console.log(flags);
+
+let checkPathConfig;
+
+getCsprojFile(flags.file)
+    .then(fixCsprojFile)
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+
+function getCsprojFile(filePath) {
+    var parser = new xml2js.Parser({
+        preserveChildrenOrder: true,
+    });
+
+    return new Promise((res, rej) => {
+        fs.readFile(path.resolve(process.cwd(), filePath), (err, data) => {
+            if (err) {
+                return rej(err);
+            }
+
+            parser.parseString(data, (parserErr, result) => {
+                if (parserErr) {
+                    return rej(err);
+                }
+
+                res(result);
+            });
+        });
+    });
+}
+
+function fixCsprojFile(csprojData) {
+    removeNotExistItem(csprojData);
+    fs.readFile(flags.configFile, (err, data) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        } else {
+            try {
+                checkPathConfig = JSON.parse(data);
+                syncFilesToCsproj(csprojData);
+            } catch(e) {
+                process.exit(1);
+            }
+        }
+    });
+}
+
+function syncFilesToCsproj(data){
+    const allIncludedFiles = [];
+    findContentItem(data, ({ filePath }) => allIncludedFiles.push(filePath));
+
+    getScanFileList()
+        .then(files => {
+            const groupedFiles =  _.groupBy(files, file => _.includes(allIncludedFiles, file));
+            const missingFilesInCsproj = groupedFiles.false;
+            if (_.isEmpty(missingFilesInCsproj)) {
+                process.exit(0);
+            }
+
+            const itemGroupForMissingFiles = findItemGroupByFilePath(data, groupedFiles.true[0]);
+
+            console.log('missing below files in csproj');
+            console.log(missingFilesInCsproj);
+
+            if (flags.autoFix) {
+                addMissingFileToCsproj(data, missingFilesInCsproj, itemGroupForMissingFiles);
+                return;
+            }
+
+            process.exit(1);
+        });
+}
+
+function removeNotExistItem(data, autoFix = false) {
+    findContentItem(data, ({ items, filePath, contentItem }) => {
+        fs.access(filePath, err => {
+            if (err) {
+                if (autoFix) {
+                    const result = _.remove(items, contentItem);
+                    console.log(`Item ${result[0].$.Include} deleted from csproj`);
+                } else {
+                    process.exit(1);
+                    console.log(`File ${filePath} is not exist`);
+                }
+            }
+        });
+    });
+}
+
+function findItemGroupByFilePath(data, filePath) {
+    const csprojDir = path.parse(path.resolve(flags.file));
+    return _.find(data.Project.ItemGroup, items => {
+        if (!items.Content) {
+            return false;
+        }
+
+        return !_.isEmpty(_.find(items.Content, content => {
+            const currentPath = path.join(csprojDir.dir, content.$.Include);
+            return currentPath === filePath;
+        }));
+    });
+}
+
+function findContentItem(data, cb) {
+    const csprojDir = path.parse(path.resolve(flags.file));
+    data.Project.ItemGroup.forEach(items => {
+        _.forEach(items.Content, contentItem => {
+            const include = contentItem.$.Include;
+            const filePath = path.join(csprojDir.dir, include);
+
+            cb({
+                items: items.Content,
+                contentItem,
+                filePath,
+            });
+        });
+    });
+
+    return data;
+}
+
+function globArrayToString(arr) {
+    return `{${arr.join(',')}}`;
+}
+
+function getScanFileList() {
+    return new Promise((res, rej) => {
+        glob(
+            globArrayToString(checkPathConfig.checkPath),
+            { ignore: checkPathConfig.ignorePath  },
+            (err, files) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res(files.map(filePath => path.join(process.cwd(), filePath)));
+                }
+            }
+        );
+    });
+}
+
+function addMissingFileToCsproj(data, missingFilesInCsproj, itemGroupForMissingFiles) {
+    const content = itemGroupForMissingFiles.Content;
+    console.log('added missing files to csproj file');
+    const projFileDir = path.parse(path.resolve(flags.file)).dir + '\\';
+
+    missingFilesInCsproj.forEach(filePath => {
+        content.push({ $: { Include: filePath.replace(projFileDir, '') } });
+    });
+
+    itemGroupForMissingFiles.Content = _.sortBy(content, contentItem => contentItem.$.Include);
+
+    saveUpdatedFile(data);
+}
+
+function saveUpdatedFile(data) {
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(data);
+
+    fs.writeFile(flags.file, xml.replace(/\/>/g, ' />').replace('&#xD;', ''), (err) => {
+        if (err) {
+            process.exit(1);
+        } else {
+            process.exit(0);
+        }
+    });
+}
